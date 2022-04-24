318cf0047e0a3fe7fed811551cca9ecdf140f53c
Leo Guagenti <43548424+LeoGuagenti@users.noreply.github.com>
2022-04-24 16:53:25 -0400

    minor changes

--- FinalProject/src/GLViewFinalProject.cpp
+++ FinalProject/src/GLViewFinalProject.cpp
@@ -36,17 +36,14 @@
 
 using namespace Aftr;
 
-GLViewFinalProject* GLViewFinalProject::New( const std::vector< std::string >& args )
-{
+GLViewFinalProject* GLViewFinalProject::New( const std::vector< std::string >& args ){
    GLViewFinalProject* glv = new GLViewFinalProject( args );
    glv->init( Aftr::GRAVITY, Vector( 0, 0, -1.0f ), "aftr.conf", PHYSICS_ENGINE_TYPE::petODE );
    glv->onCreate();
    return glv;
 }
 
-
-GLViewFinalProject::GLViewFinalProject( const std::vector< std::string >& args ) : GLView( args )
-{
+GLViewFinalProject::GLViewFinalProject( const std::vector< std::string >& args ) : GLView( args ){
    //Initialize any member variables that need to be used inside of LoadMap() here.
    //Note: At this point, the Managers are not yet initialized. The Engine initialization
    //occurs immediately after this method returns (see GLViewFinalProject::New() for
@@ -62,14 +59,11 @@ GLViewFinalProject::GLViewFinalProject( const std::vector< std::string >& args )
    //GLViewFinalProject::onCreate() is invoked after this module's LoadMap() is completed.
 }
 
-
-void GLViewFinalProject::onCreate()
-{
+void GLViewFinalProject::onCreate(){
    //GLViewFinalProject::onCreate() is invoked after this module's LoadMap() is completed.
    //At this point, all the managers are initialized. That is, the engine is fully initialized.
 
-   if( this->pe != NULL )
-   {
+   if( this->pe != NULL ){
       //optionally, change gravity direction and magnitude here
       //The user could load these values from the module's aftr.conf
       this->pe->setGravityNormalizedVector( Vector( 0,0,-1.0f ) );
@@ -79,66 +73,27 @@ void GLViewFinalProject::onCreate()
    //this->setNumPhysicsStepsPerRender( 0 ); //pause physics engine on start up; will remain paused till set to 1
 }
 
+GLViewFinalProject::~GLViewFinalProject() { /* Implicitly calls GLView::~GLView() */ }
 
-GLViewFinalProject::~GLViewFinalProject()
-{
-   //Implicitly calls GLView::~GLView()
-}
-
-
-void GLViewFinalProject::updateWorld()
-{
+void GLViewFinalProject::updateWorld() {
    GLView::updateWorld(); //Just call the parent's update world first.
                           //If you want to add additional functionality, do it after
                           //this call.
 }
 
-
-void GLViewFinalProject::onResizeWindow( GLsizei width, GLsizei height )
-{
-   GLView::onResizeWindow( width, height ); //call parent's resize method.
-}
-
-
-void GLViewFinalProject::onMouseDown( const SDL_MouseButtonEvent& e )
-{
-   GLView::onMouseDown( e );
-}
-
-
-void GLViewFinalProject::onMouseUp( const SDL_MouseButtonEvent& e )
-{
-   GLView::onMouseUp( e );
-}
-
-
-void GLViewFinalProject::onMouseMove( const SDL_MouseMotionEvent& e )
-{
-   GLView::onMouseMove( e );
-}
-
-
-void GLViewFinalProject::onKeyDown( const SDL_KeyboardEvent& key )
-{
+void GLViewFinalProject::onResizeWindow( GLsizei width, GLsizei height ){ GLView::onResizeWindow( width, height ); }
+void GLViewFinalProject::onMouseDown( const SDL_MouseButtonEvent& e ){ GLView::onMouseDown( e ); }
+void GLViewFinalProject::onMouseUp(const SDL_MouseButtonEvent& e) { GLView::onMouseUp(e); }
+void GLViewFinalProject::onMouseMove( const SDL_MouseMotionEvent& e ){ GLView::onMouseMove( e ); }
+void GLViewFinalProject::onKeyUp(const SDL_KeyboardEvent& key) { GLView::onKeyUp(key); }
+void GLViewFinalProject::onKeyDown( const SDL_KeyboardEvent& key ){
    GLView::onKeyDown( key );
    if( key.keysym.sym == SDLK_0 )
       this->setNumPhysicsStepsPerRender( 1 );
-
-   if( key.keysym.sym == SDLK_1 )
-   {
-
-   }
+   if( key.keysym.sym == SDLK_1 ){}
 }
 
-
-void GLViewFinalProject::onKeyUp( const SDL_KeyboardEvent& key )
-{
-   GLView::onKeyUp( key );
-}
-
-
-void Aftr::GLViewFinalProject::loadMap()
-{
+void Aftr::GLViewFinalProject::loadMap(){
    this->worldLst = new WorldList(); //WorldList is a 'smart' vector that is used to store WO*'s
    this->actorLst = new WorldList();
    this->netLst = new WorldList();
@@ -149,41 +104,17 @@ void Aftr::GLViewFinalProject::loadMap()
    Axes::isVisible = true;
    this->glRenderer->isUsingShadowMapping( false ); //set to TRUE to enable shadow mapping, must be using GL 3.2+
 
-   this->cam->setPosition( 15,15,10 );
+   this->cam->setPosition( 0, 75, 120 );
+   this->cam->setCameraLookAtPoint({ 0,0,0 });
 
    std::string shinyRedPlasticCube( ManagerEnvironmentConfiguration::getSMM() + "/models/cube4x4x4redShinyPlastic_pp.wrl" );
    std::string wheeledCar( ManagerEnvironmentConfiguration::getSMM() + "/models/rcx_treads.wrl" );
    std::string grass( ManagerEnvironmentConfiguration::getSMM() + "/models/grassFloor400x400_pp.wrl" );
    std::string human( ManagerEnvironmentConfiguration::getSMM() + "/models/human_chest.wrl" );
-   
-   //SkyBox Textures readily available
-   std::vector< std::string > skyBoxImageNames; //vector to store texture paths
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/sky_water+6.jpg" );
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/sky_dust+6.jpg" );
-   skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/sky_mountains+6.jpg" );
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/sky_winter+6.jpg" );
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/early_morning+6.jpg" );
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/sky_afternoon+6.jpg" );
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/sky_cloudy+6.jpg" );
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/sky_cloudy3+6.jpg" );
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/sky_day+6.jpg" );
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/sky_day2+6.jpg" );
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/sky_deepsun+6.jpg" );
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/sky_evening+6.jpg" );
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/sky_morning+6.jpg" );
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/sky_morning2+6.jpg" );
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/sky_noon+6.jpg" );
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/sky_warp+6.jpg" );
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/space_Hubble_Nebula+6.jpg" );
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/space_gray_matter+6.jpg" );
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/space_easter+6.jpg" );
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/space_hot_nebula+6.jpg" );
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/space_ice_field+6.jpg" );
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/space_lemon_lime+6.jpg" );
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/space_milk_chocolate+6.jpg" );
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/space_solar_bloom+6.jpg" );
-   //skyBoxImageNames.push_back( ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/space_thick_rb+6.jpg" );
+   std::string maze(ManagerEnvironmentConfiguration::getSMM() + "/models/maze.stl");
 
+   //std::string tableTexture(ManagerEnvironmentConfiguration::getLMM() + "models/table.jpg");
+   
    {
       //Create a light
       float ga = 0.1f; //Global Ambient Light level for this module
@@ -198,6 +129,10 @@ void Aftr::GLViewFinalProject::loadMap()
       worldLst->push_back( light );
    }
 
+   //SkyBox Textures readily available
+   std::vector< std::string > skyBoxImageNames; //vector to store texture paths
+   skyBoxImageNames.push_back(ManagerEnvironmentConfiguration::getSMM() + "/images/skyboxes/room.jpg");
+
    {
       //Create the SkyBox
       WO* wo = WOSkyBox::New( skyBoxImageNames.at( 0 ), this->getCameraPtrPtr() );
@@ -209,135 +144,65 @@ void Aftr::GLViewFinalProject::loadMap()
 
    { 
       ////Create the infinite grass plane (the floor)
-      WO* wo = WO::New( grass, Vector( 1, 1, 1 ), MESH_SHADING_TYPE::mstFLAT );
+      WO* wo = WO::New( grass, Vector( 0.75, 0.75, 0.75 ), MESH_SHADING_TYPE::mstFLAT );
       wo->setPosition( Vector( 0, 0, 0 ) );
       wo->renderOrderType = RENDER_ORDER_TYPE::roOPAQUE;
+
       wo->upon_async_model_loaded( [wo]()
          {
-            ModelMeshSkin& grassSkin = wo->getModel()->getModelDataShared()->getModelMeshes().at( 0 )->getSkins().at( 0 );
-            grassSkin.getMultiTextureSet().at( 0 )->setTextureRepeats( 5.0f );
-            grassSkin.setAmbient( aftrColor4f( 0.4f, 0.4f, 0.4f, 1.0f ) ); //Color of object when it is not in any light
-            grassSkin.setDiffuse( aftrColor4f( 1.0f, 1.0f, 1.0f, 1.0f ) ); //Diffuse color components (ie, matte shading color of this object)
-            grassSkin.setSpecular( aftrColor4f( 0.4f, 0.4f, 0.4f, 1.0f ) ); //Specular color component (ie, how "shiney" it is)
-            grassSkin.setSpecularCoefficient( 10 ); // How "sharp" are the specular highlights (bigger is sharper, 1000 is very sharp, 10 is very dull)
-         } );
-      wo->setLabel( "Grass" );
+            /*std::string tableTexturePath = ManagerEnvironmentConfiguration::getLMM() + "models/table.jpg";
+            Texture* text = ManagerTexture::loadTexture(tableTexturePath);
+            ModelMeshSkin tableSkin(text);*/
+            
+            ModelMeshSkin& tableSkin = wo->getModel()->getModelDataShared()->getModelMeshes().at(0)->getSkins().at(0);
+            tableSkin.getMultiTextureSet().at( 0 )->setTextureRepeats( 5.0f );
+            tableSkin.setAmbient( aftrColor4f( 0.4f, 0.4f, 0.4f, 1.0f ) ); //Color of object when it is not in any light
+            tableSkin.setDiffuse( aftrColor4f( 1.0f, 1.0f, 1.0f, 1.0f ) ); //Diffuse color components (ie, matte shading color of this object)
+            tableSkin.setSpecular( aftrColor4f( 0.4f, 0.4f, 0.4f, 1.0f ) ); //Specular color component (ie, how "shiney" it is)
+            tableSkin.setSpecularCoefficient( 10 ); // How "sharp" are the specular highlights (bigger is sharper, 1000 is very sharp, 10 is very dull) 
+            tableSkin.setMeshShadingType(MESH_SHADING_TYPE::mstFLAT);
+            //wo->getModel()->getSkins().push_back(tableSkin);
+
+            
+          } );
+      wo->setLabel( "Table" );
       worldLst->push_back( wo );
    }
 
-   //{
-   //   //Create the infinite grass plane that uses the Open Dynamics Engine (ODE)
-   //   WO* wo = WOStatic::New( grass, Vector(1,1,1), MESH_SHADING_TYPE::mstFLAT );
-   //   ((WOStatic*)wo)->setODEPrimType( ODE_PRIM_TYPE::PLANE );
-   //   wo->setPosition( Vector(0,0,0) );
-   //   wo->renderOrderType = RENDER_ORDER_TYPE::roOPAQUE;
-   //   wo->getModel()->getModelDataShared()->getModelMeshes().at(0)->getSkins().at(0).getMultiTextureSet().at(0)->setTextureRepeats( 5.0f );
-   //   wo->setLabel( "Grass" );
-   //   worldLst->push_back( wo );
-   //}
-
-   //{
-   //   //Create the infinite grass plane that uses NVIDIAPhysX(the floor)
-   //   WO* wo = WONVStaticPlane::New( grass, Vector( 1, 1, 1 ), MESH_SHADING_TYPE::mstFLAT );
-   //   wo->setPosition( Vector( 0, 0, 0 ) );
-   //   wo->renderOrderType = RENDER_ORDER_TYPE::roOPAQUE;
-   //   wo->getModel()->getModelDataShared()->getModelMeshes().at( 0 )->getSkins().at( 0 ).getMultiTextureSet().at( 0 )->setTextureRepeats( 5.0f );
-   //   wo->setLabel( "Grass" );
-   //   worldLst->push_back( wo );
-   //}
-
-   //{
-   //   //Create the infinite grass plane (the floor)
-   //   WO* wo = WONVPhysX::New( shinyRedPlasticCube, Vector( 1, 1, 1 ), MESH_SHADING_TYPE::mstFLAT );
-   //   wo->setPosition( Vector( 0, 0, 50.0f ) );
-   //   wo->renderOrderType = RENDER_ORDER_TYPE::roOPAQUE;
-   //   wo->setLabel( "Grass" );
-   //   worldLst->push_back( wo );
-   //}
-
-   //{
-   //   WO* wo = WONVPhysX::New( shinyRedPlasticCube, Vector( 1, 1, 1 ), MESH_SHADING_TYPE::mstFLAT );
-   //   wo->setPosition( Vector( 0, 0.5f, 75.0f ) );
-   //   wo->renderOrderType = RENDER_ORDER_TYPE::roOPAQUE;
-   //   wo->setLabel( "Grass" );
-   //   worldLst->push_back( wo );
-   //}
-
-   //{
-   //   WO* wo = WONVDynSphere::New( ManagerEnvironmentConfiguration::getVariableValue( "sharedmultimediapath" ) + "/models/sphereRp5.wrl", Vector( 1.0f, 1.0f, 1.0f ), mstSMOOTH );
-   //   wo->setPosition( 0, 0, 100.0f );
-   //   wo->setLabel( "Sphere" );
-   //   this->worldLst->push_back( wo );
-   //}
-
-   //{
-   //   WO* wo = WOHumanCal3DPaladin::New( Vector( .5, 1, 1 ), 100 );
-   //   ((WOHumanCal3DPaladin*)wo)->rayIsDrawn = false; //hide the "leg ray"
-   //   ((WOHumanCal3DPaladin*)wo)->isVisible = false; //hide the Bounding Shell
-   //   wo->setPosition( Vector( 20, 20, 20 ) );
-   //   wo->setLabel( "Paladin" );
-   //   worldLst->push_back( wo );
-   //   actorLst->push_back( wo );
-   //   netLst->push_back( wo );
-   //   this->setActor( wo );
-   //}
-   //
-   //{
-   //   WO* wo = WOHumanCyborg::New( Vector( .5, 1.25, 1 ), 100 );
-   //   wo->setPosition( Vector( 20, 10, 20 ) );
-   //   wo->isVisible = false; //hide the WOHuman's bounding box
-   //   ((WOHuman*)wo)->rayIsDrawn = false; //show the 'leg' ray
-   //   wo->setLabel( "Human Cyborg" );
-   //   worldLst->push_back( wo );
-   //   actorLst->push_back( wo ); //Push the WOHuman as an actor
-   //   netLst->push_back( wo );
-   //   this->setActor( wo ); //Start module where human is the actor
-   //}
-
-   //{
-   //   //Create and insert the WOWheeledVehicle
-   //   std::vector< std::string > wheels;
-   //   std::string wheelStr( "../../../shared/mm/models/WOCar1970sBeaterTire.wrl" );
-   //   wheels.push_back( wheelStr );
-   //   wheels.push_back( wheelStr );
-   //   wheels.push_back( wheelStr );
-   //   wheels.push_back( wheelStr );
-   //   WO* wo = WOCar1970sBeater::New( "../../../shared/mm/models/WOCar1970sBeater.wrl", wheels );
-   //   wo->setPosition( Vector( 5, -15, 20 ) );
-   //   wo->setLabel( "Car 1970s Beater" );
-   //   ((WOODE*)wo)->mass = 200;
-   //   worldLst->push_back( wo );
-   //   actorLst->push_back( wo );
-   //   this->setActor( wo );
-   //   netLst->push_back( wo );
-   //}
+   {
+      // maze object
+      WO* wo = WO::New(maze, Vector( 1, 1, 1 ), MESH_SHADING_TYPE::mstFLAT );
+      wo->setPosition( Vector( 0, 0, 50.0f ) );
+      wo->rotateAboutRelX(-90 * DEGtoRAD);
+      wo->renderOrderType = RENDER_ORDER_TYPE::roOPAQUE;
+      wo->setLabel( "Maze" );
+      worldLst->push_back( wo );
+   }
    
    //Make a Dear Im Gui instance via the WOImGui in the engine... This calls
    //the default Dear ImGui demo that shows all the features... To create your own,
    //inherit from WOImGui and override WOImGui::drawImGui_for_this_frame(...) (among any others you need).
-   WOImGui* gui = WOImGui::New( nullptr );
-   gui->setLabel( "My Gui" );
-   gui->subscribe_drawImGuiWidget(
-      [this, gui]() //this is a lambda, the capture clause is in [], the input argument list is in (), and the body is in {}
-      {
-         ImGui::ShowDemoWindow(); //Displays the default ImGui demo from C:/repos/aburn/engine/src/imgui_implot/implot_demo.cpp
-         WOImGui::draw_AftrImGui_Demo( gui ); //Displays a small Aftr Demo from C:/repos/aburn/engine/src/aftr/WOImGui.cpp
-         ImPlot::ShowDemoWindow(); //Displays the ImPlot demo using ImGui from C:/repos/aburn/engine/src/imgui_implot/implot_demo.cpp
-      } );
-   this->worldLst->push_back( gui );
+   //WOImGui* gui = WOImGui::New( nullptr );
+   //gui->setLabel( "My Gui" );
+   //gui->subscribe_drawImGuiWidget(
+   //   [this, gui]() //this is a lambda, the capture clause is in [], the input argument list is in (), and the body is in {}
+   //   {
+   //      ImGui::ShowDemoWindow(); //Displays the default ImGui demo from C:/repos/aburn/engine/src/imgui_implot/implot_demo.cpp
+   //      WOImGui::draw_AftrImGui_Demo( gui ); //Displays a small Aftr Demo from C:/repos/aburn/engine/src/aftr/WOImGui.cpp
+   //      ImPlot::ShowDemoWindow(); //Displays the ImPlot demo using ImGui from C:/repos/aburn/engine/src/imgui_implot/implot_demo.cpp
+   //   } );
+   //this->worldLst->push_back( gui );
 
    createFinalProjectWayPoints();
 }
 
-
-void GLViewFinalProject::createFinalProjectWayPoints()
-{
-   // Create a waypoint with a radius of 3, a frequency of 5 seconds, activated by GLView's camera, and is visible.
-   WayPointParametersBase params(this);
-   params.frequency = 5000;
-   params.useCamera = true;
-   params.visible = true;
-   WOWayPointSpherical* wayPt = WOWayPointSpherical::New( params, 3 );
-   wayPt->setPosition( Vector( 50, 0, 3 ) );
-   worldLst->push_back( wayPt );
+void GLViewFinalProject::createFinalProjectWayPoints(){
+    // Create a waypoint with a radius of 3, a frequency of 5 seconds, activated by GLView's camera, and is visible.
+    WayPointParametersBase params(this);
+    params.frequency = 5000;
+    params.useCamera = true;
+    params.visible = true;
+    WOWayPointSpherical* wayPt = WOWayPointSpherical::New(params, 3);
+    wayPt->setPosition(Vector(50, 0, 3));
+    worldLst->push_back(wayPt);
 }
